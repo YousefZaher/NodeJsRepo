a077157c9bd2e7380ed28d03d8b2a4a1107a6c0a
Yousef Zaher <s12325147@stu.najah.edu>
2025-03-16 06:21:30 +0200

    Node Js TESt2

--- 1-intro.ts
+++
@@ -1,8 +0,0 @@
-
-const amount: number = 9
-
-if(amount < 10)
-    console.log("small Number")
-else{
-    console.log("Large Number")
-}
--- 3-Main.ts
+++
@@ -1,7 +0,0 @@
-
-
-const things = require('./Func')
-
-const namex:string = "Yousef"
-
-console.log(things[1]())
